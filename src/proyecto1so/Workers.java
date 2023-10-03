37c03f06d94137ba08e9a99b17cc9b322d396f51
Giova2002 <cianfaglione.giovanna@correo.unimet.edu.ve>
2023-10-02 22:48:34 -0400

    First Commit

---
+++ src/proyecto1so/Workers.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto1so;
+
+/**
+ *
+ * @author giovannacianfaglione
+ */
+public class Workers extends Thread {
+    
+    
+    @Override
+    public void run(){
+        
+    }
+    
+}
